0e4ed5b9eb98b3956e58035d065dc1a54cd499ac
ajshan23 <ajmalshahan23@gmail.com>
2023-12-24 11:58:31 +0530

    fitst commit

---
+++ src/db/index.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+
+const connectDB=async()=>{
+        try {
+           const connctionInstance= await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+           console.log(`\n mongodb connected !! DB HOST:${connctionInstance.connection.host}`);
+        } catch (error) {
+            console.log("Mongodb connction error:",error);
+            process.exit(1)
+        }
+}
+
+export default connectDB
