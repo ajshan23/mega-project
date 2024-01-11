4107d512ad1cdda49c8a78eba6f7e6067af40bae
ajshan23 <ajmalshahan23@gmail.com>
2024-01-11 20:24:33 +0530

    some changes,in logout user,check unset property

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -5,8 +5,7 @@ import { User } from "../models/user.model.js";
 
 export const verifyJWT = asyncHandler(async(req, _, next) => {
     try {
-        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
-        
+        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")// this is for other platform developers
         
         if (!token) {
             throw new ApiError(401, "Unauthorized request")
